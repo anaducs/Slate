6dd21cbef75f218e2596fcb60f5fe9c08acbf3c0
Anandu C S <csanandu1998@gmail.com>
2024-12-12 23:28:32 +0530

    added validation to user login and signup page connected both to backend

--- src/Pages/Auth/Login_Signup.jsx
+++ src/Pages/Auth/Login_Signup.jsx
@@ -9,8 +9,9 @@ function Login_Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState({});
-  
-  
+  const [serverResponse, setServerResponse] = useState({});
+  const [serverResponsefS, setServerResponsefS] = useState("");
+
   //input handling
 
   const nameHandle = (event) => {
@@ -30,47 +31,66 @@ function Login_Signup() {
   };
   //form validation
 
-  const validateEmail = (email)=>{
+  const validateEmail = (email) => {
     const emailReg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailReg.test(email);
-  }
+  };
 
-  const validatePassword = (password)=>{
-    const minLength =password.length>=8;
+  const validatePassword = (password) => {
+    const minLength = password.length >= 8;
     const hasUppercase = /[A-Z]/.test(password);
     const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
     return minLength && hasUppercase && hasSpecialChar;
-
-  }
-
-  const formValidation = ()=>{
-  let validationErrors  = {};
-
-  if(!name && !isLogin) validationErrors.name="Name is required";
-  if(!email || !validateEmail(email)) validationErrors.email="inavlid email"
-  if(!password || !validatePassword(password))validationErrors.password="password must have specialcharacter and uppercase"
-  setErrors(validationErrors);
-  return Object.keys(validationErrors).length === 0;
-  }
-  //login logic
-  const userLogin = (event) => {
-    if(!formValidation())return;
-    console.log(email);
   };
 
-  //sigun logic
+  const formValidation = () => {
+    let validationErrors = {};
 
-  const userRegister = async() => {
-    if(!formValidation()) return;
-    try{
-      const response = await axios.post("http://localhost:3001/api/users/register",{
-        name,email,password
-      });
-      console.log(response);
-      
-    }catch(err){
-      console.log(err.message);
-      
+    if (!name && !isLogin) validationErrors.name = "Name is required";
+    if (!email || !validateEmail(email))
+      validationErrors.email = "inavlid email";
+    if (!password || !validatePassword(password))
+      validationErrors.password =
+        "password must have specialcharacter and uppercase";
+    setErrors(validationErrors);
+    return Object.keys(validationErrors).length === 0;
+  };
+  //login logic
+  const userLogin = async () => {
+    // if (!formValidation()) return;
+    try {
+      let errors = {};
+      const response = await axios.post(
+        "http://localhost:3001/api/users/login",
+        { email, password }
+      );
+      errors.msg = response.data.msg;
+      setServerResponse(errors);
+    } catch (err) {
+      //error
+    }
+  };
+
+  //singup logic
+
+  const userRegister = async () => {
+    if (!formValidation()) return;
+    try {
+      const response = await axios.post(
+        "http://localhost:3001/api/users/register",
+        {
+          name,
+          email,
+          password,
+        }
+      );
+    } catch (err) {
+      let errors = "";
+      if (axios.isAxiosError(err)) {
+        const stat = err.response.data.msg;
+        errors = stat;
+        setServerResponsefS(stat);
+      }
     }
   };
 
@@ -82,8 +102,7 @@ function Login_Signup() {
       {isLogin ? (
         <form action="" autoComplete="on">
           <h2>Login Now</h2>
-            <div className="inputs">
-            
+          <div className="inputs">
             <img src="/assets/email.svg" alt="" width="20px" />
             <input
               type="email"
@@ -96,7 +115,7 @@ function Login_Signup() {
           <div className="inputs mg">
             <img src="/assets/pass.svg" alt="" width="20px" />
             <input
-             name="password"
+              name="password"
               type="password"
               placeholder="password"
               value={password}
@@ -120,19 +139,22 @@ function Login_Signup() {
               type="button"
               value="Login"
               className="btn"
-              onClick={userLogin}
+              onClick={() => {
+                userLogin();
+                setTimeout(() => {
+                  setServerResponse({});
+                }, 600);
+              }}
             />
             <p>
               New User ?<span onClick={updateState}> Signup Now</span>
             </p>
-          </div>          
-          {errors && <div className="error">{errors.email||errors.password}  
-               
-          </div>}
+          </div>
+          {serverResponse && <div className="error">{serverResponse.msg}</div>}
         </form>
       ) : (
         //signup page
-        <form action="">
+        <form action="" autoComplete="on">
           <h2>Register</h2>
           <div className="inputs">
             <img src="/assets/acc.svg" alt="" width="20px" />
@@ -169,15 +191,36 @@ function Login_Signup() {
               type="button"
               value="SignUp"
               className="btn"
-              onClick={userRegister}
+              onClick={() => {
+                userRegister();
+                setTimeout(() => {
+                  setServerResponsefS("");
+                  setErrors({});
+                }, 600);
+              }}
             />
             <p>
-              Already registered ?<span onClick={updateState}> Login</span>
+              Already registered ?
+              <span
+                onClick={() => {
+                  updateState();
+                  setName("");
+                  setEmail("");
+                  setPassword("");
+                  setServerResponse({});
+                }}
+              >
+                {" "}
+                Login
+              </span>
             </p>
           </div>
-          {errors && <div className="error">{errors.name||errors.email||errors.password}  
-               
-          </div>}
+          {serverResponsefS && <div className="error"> {serverResponsefS}</div>}
+          {errors && (
+            <div className="error">
+              {errors.name || errors.email || errors.password}
+            </div>
+          )}
         </form>
       )}
     </div>
