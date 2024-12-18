ee9c3cac4170b1eaa11f1f7770f1fc351fbf5472
Anandu C S <csanandu1998@gmail.com>
2024-12-18 15:52:43 +0530

    backup

--- src/Pages/NewDocument/NewDocument.jsx
+++ src/Pages/NewDocument/NewDocument.jsx
@@ -1,27 +1,57 @@
-import React , { useState } from 'react'
-import ReactQuill from "react-quill";
-import 'react-quill/dist/quill.snow.css';
-
+import React, { useCallback, useState, useEffect } from "react";
+import { Quill } from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { io } from "socket.io-client";
 import "./NewDocument.css";
 
+const toolbarOptions = [
+  [{ size: ["small", "medium", "large", "huge"] }],
+  [{ header: "1" }, { header: "2" }, { font: [] }],
+  [{ align: [] }],
+  ["bold", "italic", "underline", "strike"],
+  [{ list: "ordered" }, { list: "bullet" }],
+  [{ indent: "-1" }, { indent: "+1" }],
+  [{ script: "sub" }, { script: "super" }],
+  ["link", "image", "video"],
+  ["blockquote", "code-block"],
+  [{ direction: "rtl" }],
+  [{ color: [] }, { background: [] }],
+  ["clean"],
+];
 
 function NewDocument() {
-const[value,setValue]=useState("");
-var toolbarOptions = [
-    [{ 'size': ['small', 'medium', 'large', 'huge'] }], 
-    [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],  
-    [{ 'align': [] }], 
-    ['bold', 'italic', 'underline', 'strike'], 
-    [{ 'list': 'ordered'}, { 'list': 'bullet' }],  
-    [{ 'indent': '-1'}, { 'indent': '+1' }],  
-    ['link', 'image', 'video'],  
-    ['blockquote', 'code-block'],  
-    [{ 'direction': 'rtl' }],  
-    [{ 'color': [] }, { 'background': [] }],  
-    ['clean'] 
-  ];
+  const [socket, setSocket] = useState(null);
+  const [quill, setQuill] = useState(null);
 
-  return <ReactQuill theme="snow" value={value} onChange={setValue} modules={{toolbar:toolbarOptions}}/>;
+  const wrapperRef = useCallback((wrap) => {
+    if (wrap == null) return;
+    wrap.innerHTML = "";
+    const editore = document.createElement("div");
+    wrap.append(editore);
+
+    const q = new Quill(editore, {
+      theme: "snow",
+      modules: { toolbar: toolbarOptions },
+    });
+    setQuill(q);
+  }, []);
+//connect to server
+  useEffect(() => {
+    const sock = io("http://localhost:5000");
+    setSocket(sock);
+
+    return () => {
+      sock.disconnect();
+    };
+  }, []);
+
+
+  //text change detect
+
+quill.on('text-change',)
+
+
+  return <div className="container" ref={wrapperRef}></div>;
 }
 
-export default NewDocument
+export default NewDocument;
